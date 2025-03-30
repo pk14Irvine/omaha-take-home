4768d43562585f8c322e144c4be7822d69f88daf
pk14Irvine <kaushikpk007@gmail.com>
2025-03-30 12:35:07 -0700

    use .env file to load db assets

--- backend/dal/engine.py
+++ backend/dal/engine.py
@@ -1,16 +1,14 @@
 from sqlalchemy import Engine
 from sqlmodel import create_engine
+from dotenv import load_dotenv
 import os
 
-# Database connection
-# DATABASE_URL = f"mysql://{os.environ.get('MYSQL_USER', 'root')}:{os.environ.get('MYSQL_PASSWORD', '')}@{os.environ.get('MYSQL_HOST', 'localhost')}/{os.environ.get('MYSQL_DB', 'climate_data')}"
-# database = databases.Database(DATABASE_URL)
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DB = os.getenv("DB")
 
-USER = "postgres"
-PASSWORD = "password"
-HOST = "localhost"
-PORT = 5432
-DB = "omaha"
-
-POSTGRES_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+POSTGRES_URL = f"postgresql://{DB_USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 engine: Engine = create_engine(POSTGRES_URL, echo=True)
